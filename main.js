f9c3a4138dd17bf341eee0158309564b85f68212
Old-G <oldg590@gmail.com>
2021-04-09 15:26:50 +0300

    update

--- main.js
+++ main.js
@@ -20,9 +20,73 @@ const scorpion = {
 }
 scorpion.attack()
 
-function createPlayer1(name, hp, img) {
-  const $player1 = document.createElement('div')
-  $player1.classList.add('player1')
+// function createPlayer1(name, hp, img) {
+//   const $player1 = document.createElement('div')
+//   $player1.classList.add('player1')
+
+//   const $progressbar = document.createElement('div')
+//   $progressbar.classList.add('progressbar')
+
+//   const $life = document.createElement('div')
+//   $life.classList.add('life')
+//   $life.style.width = hp + '%'
+
+//   const $name = document.createElement('div')
+//   $name.classList.add('name')
+//   $name.innerText = name
+
+//   const $character = document.createElement('div')
+//   $character.classList.add('character')
+
+//   const $img = document.createElement('img')
+//   $img.src = img
+
+//   $player1.appendChild($progressbar)
+//   $player1.appendChild($character)
+//   $progressbar.appendChild($life)
+//   $progressbar.appendChild($name)
+//   $character.appendChild($img)
+
+//   const $arenas = document.querySelector('.arenas')
+//   $arenas.appendChild($player1)
+// }
+// createPlayer1(subzero.name, subzero.hp, subzero.img)
+
+// function createPlayer2(name, hp, img) {
+//   const $player2 = document.createElement('div')
+//   $player2.classList.add('player2')
+
+//   const $progressbar = document.createElement('div')
+//   $progressbar.classList.add('progressbar')
+
+//   const $life = document.createElement('div')
+//   $life.classList.add('life')
+//   $life.style.width = hp + '%'
+
+//   const $name = document.createElement('div')
+//   $name.classList.add('name')
+//   $name.innerText = name
+
+//   const $character = document.createElement('div')
+//   $character.classList.add('character')
+
+//   const $img = document.createElement('img')
+//   $img.src = img
+
+//   $player2.appendChild($progressbar)
+//   $player2.appendChild($character)
+//   $progressbar.appendChild($life)
+//   $progressbar.appendChild($name)
+//   $character.appendChild($img)
+
+//   const $arenas = document.querySelector('.arenas')
+//   $arenas.appendChild($player2)
+// }
+// createPlayer2(scorpion.name, scorpion.hp, scorpion.img)
+
+function createPlayer(name, hp, img) {
+  const $player = document.createElement('div')
+  $player.classList.add('player1')
 
   const $progressbar = document.createElement('div')
   $progressbar.classList.add('progressbar')
@@ -41,45 +105,14 @@ function createPlayer1(name, hp, img) {
   const $img = document.createElement('img')
   $img.src = img
 
-  $player1.appendChild($progressbar)
-  $player1.appendChild($character)
+  $player.appendChild($progressbar)
+  $player.appendChild($character)
   $progressbar.appendChild($life)
   $progressbar.appendChild($name)
   $character.appendChild($img)
 
   const $arenas = document.querySelector('.arenas')
-  $arenas.appendChild($player1)
+  $arenas.appendChild($player)
 }
-createPlayer1(subzero.name, subzero.hp, subzero.img)
-
-function createPlayer2(name, hp, img) {
-  const $player2 = document.createElement('div')
-  $player2.classList.add('player2')
-
-  const $progressbar = document.createElement('div')
-  $progressbar.classList.add('progressbar')
-
-  const $life = document.createElement('div')
-  $life.classList.add('life')
-  $life.style.width = hp + '%'
-
-  const $name = document.createElement('div')
-  $name.classList.add('name')
-  $name.innerText = name
-
-  const $character = document.createElement('div')
-  $character.classList.add('character')
-
-  const $img = document.createElement('img')
-  $img.src = img
-
-  $player2.appendChild($progressbar)
-  $player2.appendChild($character)
-  $progressbar.appendChild($life)
-  $progressbar.appendChild($name)
-  $character.appendChild($img)
-
-  const $arenas = document.querySelector('.arenas')
-  $arenas.appendChild($player2)
-}
-createPlayer2(scorpion.name, scorpion.hp, scorpion.img)
+createPlayer(subzero.name, subzero.hp, subzero.img)
+createPlayer(scorpion.name, scorpion.hp, scorpion.img)
